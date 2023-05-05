cd4ac6089b00ca658e2b3a761f3646baa8327299
Owen Riske <owenr0129@5565.ca>
2023-05-05 08:11:46 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+//Owen Riske
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        Mp3Player test=new Mp3Player("Whomp Whomp.mp3");
+
+        test.play();
+    }
+}
